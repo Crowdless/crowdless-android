7697de8a2fbaeeb1124aabaa22eb6acba585526b
Krishan Patel <krishan@rockolabs.com>
2014-10-18 16:39:56 +0100

    implemented login

--- app/src/main/java/com/crowdless/LoginActivity.java
+++ app/src/main/java/com/crowdless/LoginActivity.java
@@ -1,39 +1,98 @@
 package com.crowdless;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity
+{
+    @InjectView(R.id.webview)
+    WebView webView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
+    @OnClick(R.id.btn_login_yammer)
+    void onLoginYammerClicked()
+    {
+        webView.clearCache(true);
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.loadUrl("https://www.yammer.com/dialog/oauth?client_id=kw9NUS1z7jWE3dNfGwAkA&redirect_uri=https://www.yammer.com");
+        webView.setVisibility(View.VISIBLE);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    void onLoggedIn(String token)
+    {
+        Log.i("Crowdless", "got token: " + token);
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putString("TOKEN", token).apply();
+        startActivity(new Intent(this, ChooseActivity.class));
+        finish();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    void onCodeRetrieved(final String code)
+    {
+        Log.i("Crowdless", "got code: " + code);
+        webView.setVisibility(View.GONE);
+
+        String url = "https://www.yammer.com/oauth2/access_token.json?client_id=kw9NUS1z7jWE3dNfGwAkA&client_secret=cdyKBt9cc7mRgSsnbIHvKDG0YKZT7Zg62mUOPviNqY&code=" + code;
+
+        final ProgressDialog dialog = ProgressDialog.show(this, null, "Logging in...");
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                Log.i("Crowdless", "got token response: " + response);
+
+                String token = null;
+                try
+                {
+                    token = response.getJSONObject("access_token").getString("token");
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                dialog.dismiss();
+                onLoggedIn(token);
+            }
+        }, null);
+        Volley.newRequestQueue(this).add(jsonObjReq);
+    }
+
+    public class CustomWebViewClient extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            Log.i("Crowdless", url);
+            if (url.startsWith("https://www.yammer.com/?code="))
+            {
+                String code = url.substring(29);
+                onCodeRetrieved(code);
+            }
+
+            return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
